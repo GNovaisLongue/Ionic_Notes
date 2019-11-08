f3742d56af065713813ec325239263d7546fa174
= <=>
2019-11-08 12:54:22 -0300

    Ionic_Notable_notes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,25 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NoteService } from './note.service';
+
+import {AngularFireModule} from 'angularfire2'
+import {AngularFireDatabaseModule} from 'angularfire2/database'
+
+import {AngularFireAuthModule} from 'angularfire2/auth'
+//será criado a seguir, é nossa classe que centraliza o acesso ao serviço de autenticação
+import {AuthService} from './auth_service'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2EuR1e4jH9pwLbiT2JEOhta_j5ojxA-g",
+  authDomain: "fatec-ipi-ionic-notes.firebaseapp.com",
+  databaseURL: "https://fatec-ipi-ionic-notes.firebaseio.com",
+  projectId: "fatec-ipi-ionic-notes",
+  storageBucket: "fatec-ipi-ionic-notes.appspot.com",
+  messagingSenderId: "999097797866",
+  appId: "1:999097797866:web:7c2ab8e8ebeaeb98a0db93"
+};
+
 
 @NgModule({
   declarations: [
@@ -14,7 +33,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +46,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    NoteService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NoteService } from '../../app/note.service';
+//import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  notes;
+  //injeção de dependência
+  //navCtrl = new NavController();
+  //é um tipo de inversão de controle
+  constructor(public navCtrl: NavController, 
+            public noteService: NoteService) {
+    //this.notes = noteService.notes;
+  }
 
+  ngOnInit (){
+    this.notes = this.noteService.fetchNotes();
+  }
+
+  onItemClick (note){
+    //console.log ("onItemClick", note);
+    this.navCtrl.push ('DetailPage', {noteParam: note});
+
+  }
+
+  onAddClick (){
+    this.navCtrl.push ('DetailPage')
   }
 
 }
